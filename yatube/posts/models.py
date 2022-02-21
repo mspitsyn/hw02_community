66bc4076dc84c9418073ddd93274242837843d8b
Максим Спицын <mgraf@yandex.ru>
2022-02-21 23:38:53 +0300

    new repository and tests

---
+++ yatube/posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+
+    def __str__(self):
+        return f'{self.title}'
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts')
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank=True, null=True, max_length=100)
