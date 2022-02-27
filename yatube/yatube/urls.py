66781d378534abb42086c2a277c001d009f3ef21
Максим Спицын <mgraf@yandex.ru>
2022-02-27 20:13:54 +0300

    fixed after review 2

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('auth/', include('users.urls', namespace='users')),
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls'))
