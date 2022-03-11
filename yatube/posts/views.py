0df80bdf4cad7cc5fd9b5955b03773686b018ace
Максим Спицын <mgraf@yandex.ru>
2022-03-11 11:07:08 +0300

    profile and post_detali 1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,30 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-from .models import Post, Group
+from .models import Post, Group, User
 
 LIMIT_POSTS = 10
 
 
+@login_required
 def index(request):
     template = 'posts/index.html'
     posts = Post.objects.all()[:LIMIT_POSTS]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, LIMIT_POSTS)
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
     context = {
         'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
 
+@login_required
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
@@ -23,3 +34,26 @@ def group_posts(request, slug):
         'posts': posts,
     }
     return render(request, template, context)
+
+
+def profile(request, username):
+    template = 'posts/profile.html'
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    paginator = Paginator(posts, LIMIT_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'author': author,
+        'posts': posts,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    context = {
+    }
+    return render(request, 'posts/post_detail.html', context)
